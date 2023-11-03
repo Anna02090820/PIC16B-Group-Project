8321877f321757ac11329ba8c0466872a1d083cb
Arely Aguirre <97066669+aaguirre321@users.noreply.github.com>
2023-11-03 09:46:16 -0700

    Update related_words_spider.py

--- playlist_generator/playlist_generator/spiders/related_words_spider.py
+++ playlist_generator/playlist_generator/spiders/related_words_spider.py
@@ -6,23 +6,36 @@ from scrapy.linkextractors import LinkExtractor
 
 class wordsSpider(scrapy.Spider):
     name = 'related_words_spider'
-    ui="bakery"
     start_urls = ["https://relatedwords.io/"] 
+
+    def _init_(self, **kwargs):
+        super()._init_(**kwargs)
+        #self.start_urls = [f'https://relatedwords.io/search.pnp?search={self.text}']
   
     def parse(self, response):
         """
         """
-        cast_url=response.url + self.ui 
-        yield scrapy.Request(cast_url,callback=self.parse_related_words)
-        #acesses the cast and crew page
-
-    def parse_related_words(self,response):
+        topic_url=response.url + "-".join((self.ui).lower().split())
+        yield scrapy.Request(topic_url,callback=self.parse_word_links)
+     
+    def parse_word_links(self, response):
+        """
+        get the links of first 10 keywords of user input's word
+        """
+        #gets links from page
+        links=self.link_extractor.extract_links(response) 
+        
+        # get the link for the first 10 related words 
+        for link in links[26:35]:
+            yield scrapy.Request(link.url, callback = self.parse_related_words)
     
+    def parse_related_words(self,response):
         
         words=response.css('span.term a::text')[0:10].getall()
+        words.append(response.css('title::text').get().split(" Words")[0].lower())
     
         for word in words:
             yield{
-                "topic":self.ui,
+                "topic": self.ui,
                 "related_word": word
             }
