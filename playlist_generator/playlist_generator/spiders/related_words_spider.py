9577caeaccb166c0da8a6acd6102b42c4277e892
Arely Aguirre <97066669+aaguirre321@users.noreply.github.com>
2023-11-02 20:19:46 -0700

    Update related_words_spider.py

--- playlist_generator/playlist_generator/spiders/related_words_spider.py
+++ playlist_generator/playlist_generator/spiders/related_words_spider.py
@@ -9,19 +9,22 @@ class wordsSpider(scrapy.Spider):
     ui="bakery"
     start_urls = ["https://relatedwords.io/"] 
   
-def parse(self, response):
-        """
-        """
-        cast_url=response.url + ui 
-        yield scrapy.Request(cast_url,callback=self.parse_related_words)
-        #acesses the cast and crew page
-
-def parse_related_words(self,response):
+    def parse(self, response):
+            """
+            """
+            cast_url=response.url + self.ui 
+            yield scrapy.Request(cast_url,callback=self.parse_word_links)
+            #acesses the cast and crew page
+     
+    def parse_word_links(self,response):
+        #get the links and yield a request to parse_related_words in each link page
     
-        words=response.css('span.term a::text')[0:10].getall()
-    
-        for word in words:
-            yield{
-                "topic":ui,
-                "related_word": word
-            }
+    def parse_related_words(self,response):
+        
+            words=response.css('span.term a::text')[0:10].getall()
+        
+            for word in words:
+                yield{
+                    "topic": self.ui,
+                    "related_word": word
+                }
