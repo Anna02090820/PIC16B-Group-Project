ccf529b4664710e3a96cbe7dcf4e4fc48507bbc4
tacosico <114280693+tacosico@users.noreply.github.com>
2023-11-02 21:35:25 -0700

    Update related_words_spider.py
    Write parse_word_links()

--- playlist_generator/playlist_generator/spiders/related_words_spider.py
+++ playlist_generator/playlist_generator/spiders/related_words_spider.py
@@ -19,8 +19,20 @@ class wordsSpider(scrapy.Spider):
         topic_url=response.url + self.ui 
         yield scrapy.Request(topic_url,callback=self.parse_word_links)
      
-    def parse_word_links(self,response):
-        #get the links and yield a request to parse_related_words in each link page
+    def parse_word_links(self, response):
+        """
+        get the links of first 10 keywords of user input's word
+        """
+        
+        # get first 10 words
+        words=response.css('span.term a::text')[0:10].getall()
+        
+        #gets links from page
+        links=self.link_extractor.extract_links(response) 
+        
+        # get the link for the first 10 related words 
+        for link in links[26:35]:
+            yield scrapy.Request(link.url, callback = self.parse_related_words)
     
     def parse_related_words(self,response):
         
