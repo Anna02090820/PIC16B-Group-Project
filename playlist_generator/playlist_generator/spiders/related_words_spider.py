2053d942327b8b148e8bcbc957592f8489304945
tacosico <acosico@g.ucla.edu>
2023-11-02 21:31:07 -0700

    Update related_words_spider.py

--- playlist_generator/playlist_generator/spiders/related_words_spider.py
+++ playlist_generator/playlist_generator/spiders/related_words_spider.py
@@ -8,23 +8,36 @@ class wordsSpider(scrapy.Spider):
     name = 'related_words_spider'
     ui="bakery"
     start_urls = ["https://relatedwords.io/"] 
+    link_extractor = LinkExtractor(allow="https://relatedwords.io/")
   
     def parse(self, response):
-            """
-            """
-            cast_url=response.url + self.ui 
-            yield scrapy.Request(cast_url,callback=self.parse_word_links)
-            #acesses the cast and crew page
+        """
+        """
+        # prob add an if statement to replace spaces with dashes
+        # add a raise exception for 404 not found
+        word_url=response.url + self.ui 
+        yield scrapy.Request(word_url, callback=self.parse_word_links)
      
-    def parse_word_links(self,response):
-        #get the links and yield a request to parse_related_words in each link page
+    def parse_word_links(self, response):
+        """
+        """
+        
+        # get first 10 words
+        words=response.css('span.term a::text')[0:10].getall()
+        
+        #gets links from page
+        links=self.link_extractor.extract_links(response) 
+        
+        # get the link for the first 10 related words 
+        for link in links[26:35]:
+            yield scrapy.Request(link.url, callback = self.parse_related_words)
     
-    def parse_related_words(self,response):
+    def parse_related_words(self, response):
         
-            words=response.css('span.term a::text')[0:10].getall()
+        words=response.css('span.term a::text')[0:10].getall()
         
-            for word in words:
-                yield{
-                    "topic": self.ui,
-                    "related_word": word
+        for word in words:
+            yield{
+                "topic": self.ui,
+                "related_word": word
                 }
