1ad4c86e49b1364302b5a980e01f1c94d8e066a9
Anna02090820 <148393779+Anna02090820@users.noreply.github.com>
2023-12-03 17:12:48 -0800

    Update consolidated_app.py

--- playlist_generator/playlist_generator/consolidated_app.py
+++ playlist_generator/playlist_generator/consolidated_app.py
@@ -233,3 +233,46 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+
+
+
+
+# related_words_spider.py
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+
+class wordsSpider(scrapy.Spider):
+    name = 'related_words_spider'
+    start_urls = ["https://relatedwords.io/"] 
+
+    def _init_(self, **kwargs):
+        super()._init_(**kwargs)
+  
+    def parse(self, response):
+        """
+        """
+        topic_url=response.url + "-".join((self.ui).lower().split())
+        yield scrapy.Request(topic_url,callback=self.parse_word_links)
+     
+    def parse_word_links(self, response):
+        """
+        get the links of first 10 keywords of user input's word
+        """
+        #gets links from page
+        related_words=response.css('span.term a::text')[0:10].getall()
+        link_list=["https://relatedwords.io/" + "-".join((word).split()) for word in related_words]
+        
+        # get the link for the first 10 related words 
+        for link in link_list:
+             yield scrapy.Request(url=link, callback = self.parse_related_words)
+    
+    def parse_related_words(self,response):
+        
+        words=response.css('span.term a::text')[0:10].getall()
+    
+        for word in words:
+            yield{
+                "topic": response.css('title::text').get().split(" Words")[0].lower(),
+                "related_word": word
+            }
