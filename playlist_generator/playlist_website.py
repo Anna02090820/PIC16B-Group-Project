a0e52df9c1ee796b6d5cd9d4297bf6d1ec6fdbd5
Arely Aguirre <97066669+aaguirre321@users.noreply.github.com>
2023-12-03 19:54:35 -0800

    Add files via upload

--- playlist_generator/playlist_website.py
+++ playlist_generator/playlist_website.py
@@ -15,7 +15,7 @@ def index():
             
             if (r.status_code == 200):
                 break
-        url=playlist.generate_playlist(keyword,topic)
+        url=playlist.generate_playlist(keyword,topic,vibe)
         return redirect(url) 
     return render_template_string('''
         <html>
