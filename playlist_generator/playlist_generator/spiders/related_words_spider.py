c24492f2c2b391bfb293b9b80170dba4c00e3bf4
Anna02090820 <148393779+Anna02090820@users.noreply.github.com>
2023-12-14 15:37:41 -0800

    Update related_words_spider.py

--- playlist_generator/playlist_generator/spiders/related_words_spider.py
+++ playlist_generator/playlist_generator/spiders/related_words_spider.py
@@ -9,17 +9,31 @@ class wordsSpider(scrapy.Spider):
     start_urls = ["https://relatedwords.io/"] 
 
     def _init_(self, **kwargs):
+        """
+        Initialize the spider with the given keyword arguments.
+
+        :param kwargs: Keyword arguments passed to the spider.
+        """
         super()._init_(**kwargs)
   
     def parse(self, response):
         """
+        Parse the initial response from the start URL and generate a request to
+        fetch related words for a given topic.
+
+        param response: The response object from the initial request to the start URL.
+        return: Yields a scrapy Request for fetching related words for a specific topic.
         """
         topic_url=response.url + "-".join((self.ui).lower().split())
         yield scrapy.Request(topic_url,callback=self.parse_word_links)
      
     def parse_word_links(self, response):
-        """
-        get the links of first 10 keywords of user input's word
+         """
+        Parse the response to extract the first 10 related words and generate requests
+        to fetch more related words for each.
+
+        param response: The response object containing the initial set of related words.
+        return: Yields scrapy Requests for each of the first 10 related words.
         """
         #gets links from page
         related_words=response.css('span.term a::text')[0:10].getall()
@@ -30,7 +44,13 @@ class wordsSpider(scrapy.Spider):
              yield scrapy.Request(url=link, callback = self.parse_related_words)
     
     def parse_related_words(self,response):
-        
+        """
+        Parse the response to extract and yield the first 10 related words 
+        from the following links.
+
+        param response: The response object from the following link, containing additional related words.
+        return: Yields a dictionary containing the 'topic' and each 'related_word'.
+        """
         words=response.css('span.term a::text')[0:10].getall()
     
         for word in words:
